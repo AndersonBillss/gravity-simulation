a878f6bb5c0e6eb8b51d42cd76bcfd2483abf9b1
AndersonBillss <anderson@thebillsfamily.com>
2026-01-02 14:06:11 -0700

    Iterate over a list of circles

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,43 @@
 use macroquad::prelude::*;
 
-struct Position {
-    x: f32,
-    y: f32,
+struct Circle {
+    position: (f32, f32),
+    velocity: (f32, f32),
 }
 #[macroquad::main("Gravity Simulation")]
 async fn main() {
     println!("Hello, gravity simulation");
-    let mut circle1_position: Position = Position { x: 30.0, y: 30.0 };
+    let mut circles: Vec<Circle> = vec![
+        Circle {
+            position: (30.0, 30.0),
+            velocity: (30.0, 30.0),
+        },
+        Circle {
+            position: (400.0, 400.0),
+            velocity: (30.0, -30.0),
+        }
+    ];
     loop {
-        circle1_position.x += 1.0;
-        circle1_position.y += 1.0;
         clear_background(BLACK);
-        draw_faded_circle(&circle1_position);
+        let dt = get_frame_time();
+        for c in &mut circles {
+            c.position.0 += c.velocity.0 * dt;
+            c.position.1 += c.velocity.1 * dt;
+            draw_faded_circle(&c.position);
+        }
         next_frame().await
     }
 }
 
-fn draw_faded_circle(circle_position: &Position) {
+fn draw_faded_circle(circle_position: &(f32, f32)) {
     let orange_1 = ORANGE;
     let orange_2: Color = Color { a: 0.8, ..ORANGE };
     let orange_3: Color = Color { a: 0.4, ..ORANGE };
     let orange_4: Color = Color { a: 0.2, ..ORANGE };
     let orange_5: Color = Color { a: 0.1, ..ORANGE };
-    draw_circle(circle_position.x, circle_position.y, 1.0, orange_1);
-    draw_circle(circle_position.x, circle_position.y, 2.0, orange_2);
-    draw_circle(circle_position.x, circle_position.y, 4.0, orange_3);
-    draw_circle(circle_position.x, circle_position.y, 6.0, orange_4);
-    draw_circle(circle_position.x, circle_position.y, 8.0, orange_5);
+    draw_circle(circle_position.0, circle_position.1, 1.0, orange_1);
+    draw_circle(circle_position.0, circle_position.1, 2.0, orange_2);
+    draw_circle(circle_position.0, circle_position.1, 4.0, orange_3);
+    draw_circle(circle_position.0, circle_position.1, 6.0, orange_4);
+    draw_circle(circle_position.0, circle_position.1, 8.0, orange_5);
 }
